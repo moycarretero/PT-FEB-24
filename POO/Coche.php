3c35abc847ce9f844fa162ba0dfc6c0cb6c8c227
Moises Carretero <moises.carretero@unidadeditorial.es>
2024-07-02 20:22:38 +0200

    Añado ejemplo de POO

--- POO/Coche.php
+++ POO/Coche.php
@@ -2,5 +2,98 @@
 
 class Coche
 {
-    
+    private string $matricula;
+    private string $marca;
+    private string $modelo;
+    private int $kilometrosRecorridos;
+    private float $cantidadCombustible;
+
+    function __construct(string $marca, string $modelo)
+    {
+        $this->marca = $marca;
+        $this->modelo = $modelo;
+        $this->cantidadCombustible = 0;
+        $this->kilometrosRecorridos = 0;
+    }
+
+    function mover(int $distancia)
+    {
+        $this->kilometrosRecorridos += $distancia;
+        $this->cantidadCombustible -= $distancia * 0.05;
+    }
+
+    function repostarCombustible(int $cantidadCombustible)
+    {
+        $this->cantidadCombustible += $cantidadCombustible;
+    }
+
+    function pasarITV()
+    {
+
+    }
+
+    public function getMatricula(): string
+    {
+        return $this->matricula;
+    }
+
+    public function setMatricula(string $matricula): void
+    {
+
+
+        $this->matricula = $matricula;
+    }
+
+    public function getMarca(): string
+    {
+        return $this->marca;
+    }
+
+    public function setMarca(string $marca): void
+    {
+
+
+        $this->marca = $marca;
+    }
+
+    public function getModelo(): string
+    {
+        return $this->modelo;
+    }
+
+    public function setModelo(string $modelo): void
+    {
+        $this->modelo = $modelo;
+    }
+
+    public function getKilometrosRecorridos(): int
+    {
+        return $this->kilometrosRecorridos;
+    }
+
+    public function setKilometrosRecorridos(int $kilometrosRecorridos): void
+    {
+        $this->kilometrosRecorridos = $kilometrosRecorridos;
+    }
+
+    public function getCantidadCombustible(): float
+    {
+        return $this->cantidadCombustible;
+    }
+
+    public function setCantidadCombustible(float $cantidadCombustible): void
+    {
+        $this->cantidadCombustible = $cantidadCombustible;
+    }
+
+    function pintaInfo()
+    {
+        echo "<ul>";
+        echo "<li>Marca: $this->marca</li>";
+        echo "<li>Modelo: $this->modelo</li>";
+        echo "<li>Km Recorridos: $this->kilometrosRecorridos</li>";
+        echo "<li>Cantidad Combustible: $this->cantidadCombustible</li>";
+        echo "</ul>";
+    }
+
 }
---
+++ POO/index.php
@@ -0,0 +1,22 @@
+<?php
+    require_once "Coche.php";
+
+    $miCoche = new Coche("Ford", "Focus");
+    $miCoche->repostarCombustible(50);
+    $miCoche->mover(400);
+    $miCoche->mover(150);
+    // miCoche.cantidadCombustible
+    // echo $miCoche->cantidadCombustible;
+    $miCoche->pintaInfo();
+
+    $miCoche->mover(1000);
+
+    $miCoche->pintaInfo();
+
+    $otroCoche = new Coche("Seat", "Ibiza");
+    $otroCoche->repostarCombustible(20);
+    $otroCoche->mover(23);
+    $otroCoche->pintaInfo();
+
+    $ferrari = new Coche("Ferrari", "Testarrosa");
+    $ferrari->pintaInfo();
